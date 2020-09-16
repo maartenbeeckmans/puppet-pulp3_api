38c1ed7694cecafca4f21dc873bcaea637a59dd6
Maarten Beeckmans <maartenb@inuits.eu>
2020-09-16 16:04:23 +0200

    initial commit

--- lib/puppet/provider/pulp3_rpm_remote/pulp3_rpm_remote.rb
+++ lib/puppet/provider/pulp3_rpm_remote/pulp3_rpm_remote.rb
@@ -2,8 +2,19 @@
 
 require 'puppet/resource_api/simple_provider'
 
+require 'rubygems'
+require 'rest-client'
+require 'json'
+
 # Implementation for the pulp3_rpm_remote type using the Resource API.
 class Puppet::Provider::Pulp3RpmRemote::Pulp3RpmRemote < Puppet::ResourceApi::SimpleProvider
+  @apiuser = 'admin'
+  @apipass = 'adminpassword'
+  @apihost = 'pulp.xxs.vagrant'
+  @apiport = '24817'
+  @uri = "http://#{@apiuser}:#{@apipass}@#{@apihost}:#{@apiport}"
+  @endpoint = '/pulp/api/v3/remotes/rpm/rpm/'
+
   def get(context)
     context.debug('Returning pre-canned example data')
     [
@@ -20,6 +31,17 @@ class Puppet::Provider::Pulp3RpmRemote::Pulp3RpmRemote < Puppet::ResourceApi::Si
 
   def create(context, name, should)
     context.notice("Creating '#{name}' with #{should.inspect}")
+    context.notice("Parameter name: #{should[:name]}")
+    data = { 'name' => should[:name],
+             'url'  => should[:url], }
+    begin
+      context.notice("The uri is #{@uri}#{@endpoint}")
+      response = RestClient.post "#{@uri}#{@endpoint}",
+        data.to_json
+      context.notice("The REST API Post response is #{response}")
+    rescue StandardError => e
+      context.err("Creating remote '#{name}' failed with: #{e}")
+    end
   end
 
   def update(context, name, should)
